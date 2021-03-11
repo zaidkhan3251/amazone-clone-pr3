39443bbea21cdbc8e346cf84d37721b00527f8b3
zaidkhan3251 <48489044+zaidkhan3251@users.noreply.github.com>
2021-03-11 22:47:25 +0500

    near to complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from "react-dom"
-import App from './App'
-import {Provider} from "react-redux" 
-import store from "./App/store"
+import App from './App';
+import './index.css';
+import reducer, { initialState } from './reducer';
+import { StateProvider } from './StateProvider';
+
 ReactDOM.render(
-<Provider store={store}>
-    <App/>
-</Provider>,document.getElementById('root'));
+    <StateProvider initialState={initialState} reducer={reducer}>
+        <App/> 
+    </StateProvider>
+
+    
+,document.getElementById('root'));
